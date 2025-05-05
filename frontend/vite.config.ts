da00e256cc4625a8724c832bfcfb66a0f9a542cf
AlexsandroVS <alexsandro.valeriano1@gmail.com>
2025-05-05 17:08:05 -0500

    v6

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
+const allowedHosts = ['localhost', 'evaluacion02-dawa-1.onrender.com']
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    host: true,
+    port: 4173,
+    allowedHosts,
+  },
 })
